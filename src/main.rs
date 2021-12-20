b0aecfef74e38a061ad4c9e810c6f8ab9045dde7
comu2e <comu2e@gmail.com>
2021-12-20 22:03:31 +0900

    ?はerrorを返すショートカット

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use std::fmt::Result;
-
-// 
 /**
  * https://rust-cli.github.io/book/tutorial/cli-args.html
  * $ grrs foobar test.txt
@@ -17,41 +14,12 @@ use std::fmt::Result;
 
 use structopt::StructOpt;
 
-fn main() {
-    // CLI引数を取得
-     // Cli構造体に値を設定
-    /*
-     * これだけでも動作するが使いにくい。
-     * --pattern="foo" or --pattern "foo"に対応できない
-     * $ cargo run pattern=1 test
-     * >> pattern=1+1
-     */
-    
-     /* sturctoptライブラリを使用する。
-     *sturctopt=0.3.13 をCargo.tomlに記載
-     * use structopt::StructOpt
-     * #[derive(StructOpt)]をstruct Cli上に記載
-     * これでCli構造体はStructOptを継承(Derive)する。
-      */
+fn main()-> Result<(),Box<dyn std::error::Error>> {
 
-    let args = Cli::from_args();
-    // Exercide for the reader:
-    //引数が正しいか確認してみる。
-    print!("{}",args.pattern);
-    print!("{:?}",args.path);
+    let content = std::fs::read_to_string("test.xtx")?;
+          println!("file content: {}",content);
+          Ok(())
     
-    //read_to_stringはstringを返さない代わりに、Resultオブジェクトを返す。
-    //Resultは今回であればStringか、std::io::Error型を返す。
-    // matchを使うことで対応できる。
-    let result = std::fs::read_to_string("test.txt");
-    //下記のように書く。
-    
-    let content = match result {
-        Ok(content) => {content},
-        Err(error) => {panic!("Oh no:{}",error);}
-    };
-    
- 
 }
 
 /*
