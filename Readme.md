fd4b42b850b1844652df432111cb2d0628110f5c
comu2e <comu2e@gmail.com>
2021-12-21 23:37:05 +0900

    色つけ表示対応

--- Readme.md
+++ Readme.md
@@ -4,8 +4,26 @@ https://rust-cli.github.io/book/tutorial/index.html
 # Version
 cargo 1.56.0
 
-# コマンド引数の取得方法
+# コマンド引数の取得方
 ```
 std::env:nth()
 ```
+上記でもいいが下記のように書くのがいい。
+
+```use structopt::StructOpt;
+#[derive(StructOpt)]
+struct Cli{
+    pattern: String ,
+    #[structopt(parse(from_os_str))]
+    path::std::path::Path::Buf
+}
+
+fn main(){
+ let content = std::fs::read_to_string()
+}
+```
+
 # test
+
+# 動作方法
+```$ cargo run dep Cargo.toml```
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-use structopt::StructOpt;
-use failure::ResultExt;
 use exitfailure::ExitFailure;
-
+use failure::ResultExt;
+use structopt::StructOpt;
+use ansi_term::Colour;
 #[derive(StructOpt)]
 struct Cli {
-  pattern : String,
+  pattern: String,
   #[structopt(parse(from_os_str))]
-  path:std::path::PathBuf,
+  path: std::path::PathBuf,
 }
 
-fn main() -> Result<(),ExitFailure>{
+fn main() -> Result<(), ExitFailure> {
   let args = Cli::from_args();
 
-  let content = std::fs::read_to_string(&args.path).with_context(|_| format!("Could not read file :{:?}",&args.path ))?;
-  for line in content.lines(){
-    if line.contains(&args.pattern){
-      println!("{}",line);
+  let content = std::fs::read_to_string(&args.path)
+    .with_context(|_| format!("Could not read file :{:?}", &args.path))?;
+  for line in content.lines() {
+    if line.contains(&args.pattern) {
+      println!("{}", Colour::Red.paint(line));
+    }else{
+      println!("{}",Colour::Green.paint(line));
     }
   }
   Ok(())
